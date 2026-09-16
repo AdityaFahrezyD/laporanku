3340cd1e6f482b705e4c153982f1b9e602bb16c1
Aditya Fahrezy <aditgamer01@gmail.com>
2026-09-16 18:25:17 +0700

    pixel expand

--- config/attachments.php
+++ config/attachments.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'max_kb' => (int) env('ATTACHMENT_MAX_KB', 5120),
-    'max_pixels' => (int) env('ATTACHMENT_MAX_PIXELS', 10000000),
+    'max_kb' => (int) env('ATTACHMENT_MAX_KB', 7120),
+    'max_pixels' => (int) env('ATTACHMENT_MAX_PIXELS', 20000000),
     'max_dimension' => (int) env('ATTACHMENT_MAX_DIMENSION', 1920),
     'quality' => (int) env('ATTACHMENT_AVIF_QUALITY', 60),
     'speed' => (int) env('ATTACHMENT_AVIF_SPEED', 8),
